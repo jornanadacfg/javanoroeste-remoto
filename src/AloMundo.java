3e02199c15f8258b60bb8d14007bd793e3923ba9
carlos <carlosfgo@gmail.com>
2018-01-18 11:22:23 -0200

    Projeto Java Noroeste

---
+++ src/AloMundo.java
@@ -0,0 +1,13 @@
+      //Programa em Java
+
+      public class AloMundo {
+
+            //O método main inicia a execução do aplicativo Java
+            public static void main ( String args[] )
+            {
+                  System.out.println("Ola para programacao Java!");
+
+            }//Fim do método main
+
+
+      }//fim da classe AloMundo
